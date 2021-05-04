3d554c89aa0969fbc6d5c1afe2a27df8abf5aa26
Vikram Viswanathan <vviswanathan@newtrax.com>
2021-05-04 07:35:12 -0400

    Project creation correction.

--- my-books/obj/Debug/net5.0/my-books.AssemblyInfo.cs
+++ my-books/obj/Debug/net5.0/my-books.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("abbde42b-ff93-4986-ab64-bc4aa11edd78")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
