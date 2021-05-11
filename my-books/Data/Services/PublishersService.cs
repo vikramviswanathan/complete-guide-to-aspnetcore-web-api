b5867e1ff52a5136a93b731056823cb656bd6169
Vikram Viswanathan <vviswanathan@newtrax.com>
2021-05-11 01:41:05 -0400

    Getting books of a publisher including authors.

--- my-books/Data/Services/PublishersService.cs
+++ my-books/Data/Services/PublishersService.cs
@@ -25,5 +25,22 @@ namespace my_books.Data.Services
             _context.Publishers.Add(_publisher);
             _context.SaveChanges();
         }
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _pulisherData = _context.Publishers
+                                            .Where(n => n.Id == publisherId)
+                                            .Select(n => new PublisherWithBooksAndAuthorsVM()
+                                            {
+                                                Name = n.Name,
+                                                BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                                                {
+                                                    BookName = n.Title,
+                                                    BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()
+                                                }).ToList()
+                                            }).FirstOrDefault();
+
+            return _pulisherData;
+        }
     }
 }
