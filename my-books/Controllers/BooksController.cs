5e1d75542759d8905052a109c5d58c891e053a00
Vikram Viswanathan <vviswanathan@newtrax.com>
2021-05-11 01:03:32 -0400

    Added Relational data with HttpPost.

--- my-books/Controllers/BooksController.cs
+++ my-books/Controllers/BooksController.cs
@@ -19,10 +19,10 @@ namespace my_books.Controllers
             _booksService = booksService;
         }
 
-        [HttpPost("add-book")]
+        [HttpPost("add-book-with-authors")]
         public IActionResult AddBook([FromBody] BookVM book)
         {
-            _booksService.AddBook(book);
+            _booksService.AddBookWithAuthors(book);
             return Ok();
         }
 
