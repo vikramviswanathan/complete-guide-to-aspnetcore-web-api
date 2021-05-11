c781e0df98a54e0c52b2b12b3d6905b5592848aa
Vikram Viswanathan <vviswanathan@newtrax.com>
2021-05-11 01:59:39 -0400

    Deleting relational data.

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -33,5 +33,12 @@ namespace my_books.Controllers
             var _response = _publishersService.GetPublisherData(id);
             return Ok(_response);
         }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            _publishersService.DeletePublisherId(id);
+            return Ok();
+        }
     }
 }
